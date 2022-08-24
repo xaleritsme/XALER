fd9c5696a2ea365bc90a5a6d3b4630e1d64cc488
Olek <Olek@DESKTOP-AOH4A5J>
2022-08-24 18:01:11 +0200

    Added social media icons

---
+++ Controllers/MyMusicController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XALER.Controllers
+{
+    public class MyMusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
